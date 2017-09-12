4a12c1b5614eba5ab77b0411479180549182102c
morgacoo <morgacoo@gmail.com>
2017-09-12 11:08:41 -0500

    First blogger commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles #added per tutorial
+
+end
+
+
+#Tutorial said first line would look like "Blogger::Application.routes.draw do"
