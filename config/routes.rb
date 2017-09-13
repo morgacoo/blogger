9c5c1ccd712645df504c5790dfd11d23177e7f77
morgacoo <morgacoo@gmail.com>
2017-09-13 12:37:11 -0500

    Finished comments section

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles #added per tutorial
-
+  resources :articles do #added per tutorial
+    resources :comments
+  end
 end
 
 
