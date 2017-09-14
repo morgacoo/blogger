44696a9136d0a4bdbb5b51039ca482d9bbd1ebac
morgacoo <morgacoo@gmail.com>
2017-09-14 09:54:14 -0500

    Finished tags - adding and removing

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,18 @@
 class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tag = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.title}' was deleted."
+
+    redirect_to article_path
+  end
 end
